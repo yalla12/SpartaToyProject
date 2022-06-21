d06052167386c7db16190a065d206617ed101e9b
sunho <yalla12@naver.com>
2022-06-21 16:53:36 +0900

    test

--- app.py
+++ app.py
@@ -11,17 +11,21 @@ app = Flask(__name__)
 # rendering (html 파일 넘겨주기)
 @app.route('/')
 def home():
-    return render_template('mainPage.html')
+    return render_template('mainpage.html')
 
 
 @app.route('/header')
 def header():
-    return render_template('header.html')
+    return render_template('header_sunho.html')
 
 
 @app.route('/footer')
 def footer():
     return render_template('footer.html')
+
+@app.route('/test')
+def test():
+    return render_template('mypage.html')
 
 
 if __name__=='__main__':
